cea4a3deaab0d1a174ca5780246bb3a7e7c263c7
James Golick <james@giraffesoft.ca>
2009-07-08 09:20:34 -0400

    Initial commit to per_mailer_smtp_settings.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'per_mailer_smtp_settings'
+
+class Test::Unit::TestCase
+end
